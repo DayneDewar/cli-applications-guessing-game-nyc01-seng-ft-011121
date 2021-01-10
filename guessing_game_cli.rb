2cca1159b042fe1cd4b3b1715f54406536d727b9
Dayne Dewar <daynedewar@yahoo.com>
2021-01-10 01:56:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@ def run_guessing_game
   
   user_input = gets.chomp
   if user_input == random
-    puts "You guessed the correct number!"
+    p "You guessed the correct number!"
   elsif user_input == "exit"
-    puts "Goodbye!"
+    p "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{random}."
+    p "Sorry! The computer guessed #{random}."
   end
 end
