4b79bd5a718e3949ede2c1c91ffa21b3e5505ef7
Dayne Dewar <daynedewar@yahoo.com>
2021-01-10 01:59:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,13 +2,6 @@ def run_guessing_game
   random = rand(1..6)
   
   user_input = gets.chomp
-  if user_input = random
-    p "You guessed the correct number!"
-  end
-  if user_input == "exit"
-    puts "Goodbye!"
-  end
-  if user_input != random
-    puts "Sorry! The computer guessed #{random}."
-  end
+  while user_input != random
+    puts 
 end
